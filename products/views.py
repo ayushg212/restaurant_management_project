8cab80db214be2b7fa5497fce951249c69c8169a
ayushg212 <57093373+ayushg212@users.noreply.github.com>
2025-08-11 15:04:02 +0530

    28877

--- products/views.py
+++ products/views.py
@@ -24,3 +24,13 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def menu_list(request):
+    # hardcoded menu items
+    menu_items = [
+        {'name': 'Margherita Pizza','price': '199','desc': 'Classic cheese & tomato'},
+        {'name': 'Veg Burger', 'price': '149', 'desc': 'Patty with fresh veggies'},
+        {'name': 'Pasta Alferfo', 'price': '129', 'desc': 'Creamy garalic sauce'},
+    ]
+    return render(request, 'products/menu.html', {'menu_items': menu_items})
