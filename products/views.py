c0658f261198a4604083d691a86971b5572646da
ayushg212 <57093373+ayushg212@users.noreply.github.com>
2025-08-16 14:43:14 +0530

    33493

--- products/views.py
+++ products/views.py
@@ -4,7 +4,9 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .models import Item
-from .serializers import ItemSerializer
+from .serializers import ItemSerializer, MenuItemSerializer
+from rest_framework.decorators import api_view
+from .models import MenuItem
 
 '''
 NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
@@ -34,3 +36,24 @@ def menu_list(request):
         {'name': 'Pasta Alferfo', 'price': '129', 'desc': 'Creamy garalic sauce'},
     ]
     return render(request, 'products/menu.html', {'menu_items': menu_items})
+
+
+
+@api_view(['GET','POST'])
+def menuitem_list_create(request):
+    try:
+        if request.method == 'GET':
+            qs = MenuItem.objects.all().order_by('-created_at')
+            rid = request.query_params.get('restaurant')
+            if rid: qs = qs.filter(restaurant_id = rid)
+            return Response(MenuItemSerializer(qs,many= True).data)
+
+        s = MenuItemSerializer(data = request.data)
+        s.is_valid(raise_exception = True)
+        s.save()
+        return Response(s.data, status = status.HTTP_201_CREATED)
+
+    except DatabaseError:
+        return Response({'detail':'db error'}, status = status.HTTP_500_INTERNAL_SERVER_ERROR))
+    except Exception:
+        return Response('detail': 'server error', status = status.HTTP_500_INTERNAL_SERVER_ERROR)
