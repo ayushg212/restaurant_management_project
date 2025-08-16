fd5c23f9cabead2b84999795ca8f347f04e2ed10
ayushg212 <57093373+ayushg212@users.noreply.github.com>
2025-08-16 15:37:48 +0530

    33489

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('reservations/',reservations,name = 'reservations'),
     path('contact/', contact, name = 'contact'),
 ]
