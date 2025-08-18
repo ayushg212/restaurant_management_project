f0119c6b9ff6de416ae9742b07f47f8422e78247
ayushg212 <57093373+ayushg212@users.noreply.github.com>
2025-08-18 13:14:15 +0530

    34504

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
